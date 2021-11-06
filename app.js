2e300017908d59bfd594d91f85b8d28d627144aa
Somnath Sardar <githubsomnath1996@gmail.com>
2021-11-06 11:01:59 +0530

    app.js and weather.js files are modified

--- app.js
+++ app.js
@@ -2,16 +2,12 @@ const weather = require("./utils/weather");
 
 weather("Kolkata", (err, res) => {
   if (err) {
-    console.log("Unable to connect to weatherstack.com");
+    console.log(err);
   } else {
-    if (res.body.error) {
-      console.log("Invalid location provided. Please check and try again.");
-    } else {
-      const data = res.body;
-      const currentWeatherData = data.current;
-      console.log(
-        `${currentWeatherData.weather_descriptions[0]}. It is currently ${currentWeatherData.temperature} degrees out. But feels like ${currentWeatherData.feelslike} degrees out.`
-      );
-    }
+    const data = res.body;
+    const currentWeatherData = data.current;
+    console.log(
+      `${currentWeatherData.weather_descriptions[0]}. It is currently ${currentWeatherData.temperature} degrees out. But feels like ${currentWeatherData.feelslike} degrees out.`
+    );
   }
 });
--- utils/weather.js
+++ utils/weather.js
@@ -5,7 +5,15 @@ const weather = (location, callback) => {
   location = encodeURIComponent(location);
   const url = `http://api.weatherstack.com/current?access_key=${constant.weather_api_key}&query=${location}`;
   request({ url, json: true }, (err, res) => {
-    callback(err, res);
+    if (err) {
+      callback("Unable to connect to weatherstack.com", undefined);
+    } else {
+      if (res.body.error) {
+        callback("Invalid location provided. Please check and try again.", undefined);
+      } else {
+        callback(undefined, res);
+      }
+    }
   });
 };
 
