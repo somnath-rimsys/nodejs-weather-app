f034605143400a7627945c5ea15415b052546b44
Somnath Sardar <githubsomnath1996@gmail.com>
2021-11-06 15:44:06 +0530

    App is configured for command line

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
+const yargs = require("yargs");
 const weather = require("./utils/weather");
 
-weather("Kolkata", (err, res) => {
-  if (err) {
-    console.log(err);
-  } else {
-    const data = res.body;
-    const currentWeatherData = data.current;
-    console.log(
-      `${currentWeatherData.weather_descriptions[0]}. It is currently ${currentWeatherData.temperature} degrees out. But feels like ${currentWeatherData.feelslike} degrees out.`
-    );
-  }
-});
+yargs(process.argv.splice(2))
+  .command({
+    command: "weather",
+    describe: "Get weather details",
+    builder: {
+      location: {
+        demandOption: true,
+        type: "string",
+      },
+    },
+    handler: (argv) => {
+      weather(argv.location, (err, res) => {
+        if (err) {
+          console.log(err);
+        } else {
+          const data = res.body;
+          const currentWeatherData = data.current;
+          console.log(
+            `${currentWeatherData.weather_descriptions[0]}. It is currently ${currentWeatherData.temperature} degrees out. But feels like ${currentWeatherData.feelslike} degrees out.`
+          );
+        }
+      });
+    },
+  })
+  .parse();
