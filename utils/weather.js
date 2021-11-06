4ff4f832830658ec1d99ea686717dd5e0f8ab5ec
Somnath Sardar <githubsomnath1996@gmail.com>
2021-11-06 15:15:17 +0530

    weather.js file updated

--- utils/weather.js
+++ utils/weather.js
@@ -5,15 +5,9 @@ const weather = (location, callback) => {
   location = encodeURIComponent(location);
   const url = `http://api.weatherstack.com/current?access_key=${constant.weather_api_key}&query=${location}`;
   request({ url, json: true }, (err, res) => {
-    if (err) {
-      callback("Unable to connect to weatherstack.com", undefined);
-    } else {
-      if (res.body.error) {
-        callback("Invalid location provided. Please check and try again.", undefined);
-      } else {
-        callback(undefined, res);
-      }
-    }
+    if (err) callback("Unable to connect to weatherstack.com", undefined);
+    else if (res.body.error) callback("Unable to find loaction.", undefined);
+    else callback(undefined, res);
   });
 };
 
